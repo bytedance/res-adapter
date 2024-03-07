050444132fc6cad2585a86f696d6c674385055f2
chengjiaxiang <chengjiaxiang@bytedance.com>
2024-03-07 19:07:37 +0800

    Disable resnorm loader for resolution interpolation

--- resadapter/model_loader.py
+++ resadapter/model_loader.py
@@ -20,30 +20,16 @@ from safetensors import safe_open
 def load_resadapter(pipeline, config):
     NORM_WEIGHTS_NAME = "resolution_normalization.safetensors"
     LORA_WEIGHTS_NAME = "resolution_lora.safetensors"
+
+    # TODO: We open the following codes, after providing weights of resolution extrapolation.
     # Load resolution normalization
-    norm_state_dict = {}
-    with safe_open(os.path.join(config.res_adapter_model, NORM_WEIGHTS_NAME), framework="pt", device="cpu") as f:
-        for key in f.keys():
-            norm_state_dict[key] = f.get_tensor(key)
-    m, u = pipeline.unet.load_state_dict(norm_state_dict, strict=False)
+    # norm_state_dict = {}
+    # with safe_open(os.path.join(config.res_adapter_model, NORM_WEIGHTS_NAME), framework="pt", device="cpu") as f:
+    #     for key in f.keys():
+    #         norm_state_dict[key] = f.get_tensor(key)
+    # m, u = pipeline.unet.load_state_dict(norm_state_dict, strict=False)
     
     # Load resolution lora
     pipeline.load_lora_weights(os.path.join(config.res_adapter_model, LORA_WEIGHTS_NAME), adapter_name="res_adapter")
-
-    return pipeline
-
-# Load resadapter for mini-example
-def load_resadapter_mini(pipeline, resadapter_path):
-    NORM_WEIGHTS_NAME = "resolution_normalization.safetensors"
-    LORA_WEIGHTS_NAME = "resolution_lora.safetensors"
-    # Load resolution normalization
-    norm_state_dict = {}
-    with safe_open(os.path.join(resadapter_path, NORM_WEIGHTS_NAME), framework="pt", device="cpu") as f:
-        for key in f.keys():
-            norm_state_dict[key] = f.get_tensor(key)
-    m, u = pipeline.unet.load_state_dict(norm_state_dict, strict=False)
-    
-    # Load resolution lora
-    pipeline.load_lora_weights(os.path.join(resadapter_path, LORA_WEIGHTS_NAME), adapter_name="res_adapter")
 
     return pipeline
